a4218442dca3bc91355d85a5e59df0e470ba080a
Daniel Braun <daniel.braun@tuta.io>
2025-04-14 20:30:23 +0200

    added TestSeparateExport.java, adjusted to fit workflow

---
+++ src/test/java/org/mal_lang/examplelang/test/TestSeparateExport.java
@@ -0,0 +1,93 @@
+package org.mal_lang.examplelang.test;
+
+import core.*;
+//import mal.*;
+import core.coverage.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+/* *
+ * TestSeparateExport
+ */
+@ExtendWith(CoverageExtension.class)
+public class TestSeparateExport extends ExampleLangTest {
+	@RegisterExtension
+	public CoverageExtension cove = new CoverageExtension(new JSONTarget());
+
+	@RegisterExtension
+	public CoverageExtension cove2 = new CoverageExtension(new ConsoleTarget());
+
+	private static Host machine;
+	private static User user;
+	private static Password password;
+
+	// enable encryption
+	public static boolean isEncryptedEnabled = true;
+
+	/*
+	private static class ExampleLangModel {
+		private static Network internet = new Network("internet");
+		private static Host machine = new Host("server");
+		private static Password password123 = new Password("password123", isEncryptedEnabled);
+
+		public ExampleLangModel() {
+			internet.addHosts(server);
+			server.addPasswords(password123);
+		}
+	}
+	 */
+
+	@BeforeAll
+	public static void setup() {
+		machine = new Host("PC");
+		user = new User("User");
+		password = new Password("qwerty", isEncryptedEnabled);
+		
+		machine.addPasswords(password);
+		password.addUser(user);
+	}
+
+	@BeforeEach
+	public void clearMdl() {
+		new Attacker().reset();
+	}
+
+	/*
+	method from ExampleLangTest.java
+	shown for comprehensibility purposes
+
+	@AfterEach
+	public void deleteModel() {
+		Asset.allAssets.clear();
+		AttackStep.allAttackSteps.clear();
+		Defense.allDefenses.clear();
+	}
+	 */
+
+	@AfterAll
+	public static void clear() {
+		password.encrypted.defaultValue = isEncryptedEnabled;
+	}
+
+	@Test
+	public void testDefDisable() {
+		password.encrypted.defaultValue = false;
+		Attacker a = new Attacker();
+		a.addAttackPoint(machine.connect);
+		a.attack();
+	}
+
+	@Test
+	public void testDefEnabled() {
+		password.encrypted.defaultValue = true;
+		Attacker a = new Attacker();
+		a.addAttackPoint(machine.connect);
+		a.attack();
+	}
+}
